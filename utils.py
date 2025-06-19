ad7b006aad476ddb68939870220ec197ef77ff04
Steklish <anton.kozlov.mailbox@gmail.com>
2025-06-19 17:46:38 +0300

    фанфакт утилита

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from thefuzz import process
-
+import requests
 
 def find_closest_match(query: str, choices: list[str]) -> str:
     """
@@ -19,6 +19,15 @@ def find_closest_match(query: str, choices: list[str]) -> str:
     best_match = process.extractOne(query, choices)[0]
     
     return best_match
+
+def get_fun_fact():
+
+    response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    if response.status_code == 200:
+        fact = response.json().get('text')
+        return fact
+    else:
+        return "Failed to get inspiration"
 
 
 if __name__ == "__main__":
