5e215501d8d709310ca4726eb844dfe96024c5f2
Steklish <anton.kozlov.mailbox@gmail.com>
2025-06-19 19:31:54 +0300

    заруба

--- game.py
+++ game.py
@@ -28,8 +28,11 @@ class Game:
         self.chapter = ChapterLogicFight(
             context = self.context,
             characters = [
-                self.generator.generate(Character, "Борис Квадробер with full hp (50 hp) (player character) кот, которого ведьма превратила в человека (ведьма была его хозяйкой, от которой он позже сбежал), на нем него только тряпка вокруг члена и ничего больше. Среднее телосложение, черные кошачьи ушки. Нижние ноги скорее кошачь, чем человеческие. Густая шерсть по всему телу. Усы на лице. Хвост черный. У него гетерохромя с ожним глазом зеленым, а вторым синим. У него острые зубы и когти. Класс персонажа - плут(способности соответствующие). Любит рыбу и не любит молоко. Отзывается на кличку 'Барсик' - сокрощенно от 'Борис'.", self.context, "Russian"),
-                self.generator.generate(Character, "Яша Лава with full hp (50 hp) random inventory (non-player character)", self.context, "Russian"),
+                self.generator.generate(Character, "Антон сын Олега, вор, который не хочет воровать,только если это очень нужно команде и/или ему. у него средний рост, тощее телосложение. Бывалый капитан пиратского корабля, который был продан за 40 гривень. Очень ловкий на корабле во время плаванья, но имеет проблемы с координацией на суше. Предпочитает лёгкую броню и оружие на цепи, взял ипотеку на новый корабль, и пока он её не выплатит не хочет умирать и впадает в ярость, увеличивающую его характеристики в двое при низком уровне здоровья (при менее 10 хп). Всего имеет 40 hp.", self.context, "Russian"),
+                self.generator.generate(Character, "Яша Лава - ЛАвовый голем with full hp (50 hp) random inventory (non-player character)", self.context, "Russian"),
+                self.generator.generate(Character, "Яша Лужа - Водяной голем with full hp (50 hp) random inventory (non-player character)", self.context, "Russian"),
+                self.generator.generate(Character, "DEADус SUS - боевой дворф with full hp (50 hp) random inventory (player character)", self.context, "Russian"),
+                self.generator.generate(Character, "ГОловка А.А - боевой опездол with full hp (50 hp) random inventory (player character)", self.context, "Russian"),
                 # self.generator.generate(Character, "random monster with full hp (50 hp) and some magic spells (enemy NPC)", self.context, "Russian")
             ]
         )
